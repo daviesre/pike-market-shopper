cba402ee930c5bcad0545567d2b507865578f5f6
RubyBe and Russ Davies <rubybe+russ@>
2016-09-07 14:18:48 -0700

    click on image to show description and shop

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -2,10 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  areDetailsShowing: false,
 
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
     },
+    showDetails: function() {
+      this.set('areDetailsShowing', true);
+    },
+    hideDetails: function() {
+      this.set('areDetailsShowing', false);
+    }
   }
 });
